f2c57e6b24e7af5d8bf5ced0fbdcfd458e9e6dff
JeffreyChan <jeffrey2212@gmail.com>
2023-05-07 02:33:46 +0800

    adding fadein

--- main.py
+++ main.py
@@ -3,17 +3,10 @@ from PIL import Image
 from add_signature import add_signature, add_sequence_number
 from sildeshow import create_video_slideshow
 from datetime import datetime
-import os 
+import os
 
-def main():
-    parser = argparse.ArgumentParser(description="Create a video slideshow from images.")
-    parser.add_argument("-input", default="./input/", help="Input folder containing the images.")
-    parser.add_argument("-output", default="./output/", help="Output folder for the signed images and video.")
-    args = parser.parse_args()
-
-    input_path = args.input
-    output_path = args.output
-    signed_path = os.path.join(output_path, 'signed/')
+def main(input_path, output_path):
+    signed_path = output_path + 'signed/'
 
     if not os.path.exists(signed_path):
         os.mkdir(signed_path)
@@ -24,20 +17,27 @@ def main():
     num_digits = len(str(total_files))
 
     seq = 1
-    signed_and_numbered_image = []
-    
+    signed_and_numbered_images = []
+
     for file in image_files:
         img = Image.open(file)
         formatted_seq = str(seq).zfill(num_digits)
         signed_image = add_signature(img, 'Casted by AI Caster.mo', './font/NotoSerif-Italic.ttf', int(max(img.size) * 0.02))
         numbered_image = add_sequence_number(signed_image, formatted_seq, './font/NotoSerif-Bold.ttf', int(max(img.size) * 0.05))
-        numbered_image.save(os.path.join(signed_path, f"{formatted_seq}.png"))
+        numbered_image.save(signed_path + formatted_seq + ".png")
         seq += 1
-        signed_and_numbered_image.append(os.path.join(signed_path, f"{formatted_seq}.png"))
+        signed_and_numbered_images.append(signed_path + formatted_seq + ".png")
 
     date_str = datetime.now().strftime("%Y%m%d")
     output_file = f"{date_str}.mp4"
-    video = create_video_slideshow(signed_and_numbered_image, os.path.join(output_path, output_file), 5)
+    video = create_video_slideshow(signed_and_numbered_images, output_path + output_file, 3, resolution=(1920, 1080))
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Create a video slideshow with signatures and sequence numbers.')
+    parser.add_argument('-input', type=str, default='./input/', help='Path to the input folder containing the images.')
+    parser.add_argument('-output', type=str, default='./output/', help='Path to the output folder where the video will be saved.')
+
+    args = parser.parse_args()
+
+    main(args.input, args.output)
--- sildeshow.py
+++ sildeshow.py
@@ -1,36 +1,28 @@
-from moviepy.editor import CompositeVideoClip, ImageClip, concatenate_videoclips
-from moviepy.video.fx.all import resize
+from moviepy.editor import ImageClip, CompositeVideoClip, concatenate_videoclips
+from moviepy.video.fx.all import resize, fadeout, fadein 
 
-def crossfade_transition(clip1, clip2, transition_duration=1):
-    clip1_end = clip1.duration - transition_duration
-    clip2_start = clip2.set_start(clip1_end)
-    return CompositeVideoClip([clip1, clip2_start])
+def resize_and_center_image(image_clip, target_resolution):
+    aspect_ratio = float(image_clip.w) / float(image_clip.h)
+    if aspect_ratio >= 1:  # Landscape or square image
+        new_clip = image_clip.fx(resize, width=target_resolution[0])
+    else:  # Portrait image
+        new_clip = image_clip.fx(resize, height=target_resolution[1])
 
-def create_video_slideshow(image_files, output_path, image_duration=3, resolution=(1920, 1080), transition_duration=2):
-    
-    def resize_and_center_image(image_clip, target_resolution):
-        aspect_ratio = float(image_clip.w) / float(image_clip.h)
-        if aspect_ratio >= 1:  # Landscape or square image
-            new_clip = image_clip.fx(resize, width=target_resolution[0])
-        else:  # Portrait image
-            new_clip = image_clip.fx(resize, height=target_resolution[1])
-        
-        # Center the image
-        new_clip = new_clip.set_position(('center', 'center'))
-        return new_clip
+    # Center the image
+    new_clip = new_clip.set_position(('center', 'center'))
+    return new_clip
+
+def create_video_slideshow(image_files, output_path, image_duration=3, resolution=(1920, 1080), transition_duration=1):
     
     clips = [resize_and_center_image(ImageClip(img_path).set_duration(image_duration), resolution) for img_path in image_files]
-    final_clips = []
 
+    final_clips = []
     for i in range(len(clips) - 1):
         clip1 = clips[i]
-        clip2 = clips[i+1]
+        clip2 = clips[i+1].fadein(transition_duration).set_start(clip1.duration - transition_duration)
+        final_clips.append(CompositeVideoClip([clip1, clip2]))
 
-        # Apply crossfade transition between two clips
-        transition = crossfade_transition(clip1, clip2, transition_duration)
-        final_clips.append(transition)
+    final_clips.append(fadeout(clips[-1], transition_duration))
 
-    final_clips.append(clips[-1])  # Append the last clip without a transition
     final_clip = concatenate_videoclips(final_clips, method='compose', padding=-transition_duration)
     final_clip.write_videofile(output_path, fps=24)
-
