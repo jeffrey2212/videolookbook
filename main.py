18732e90d79fb4d1794aafc5371baeaf14d9df11
JeffreyChan <jeffrey2212@gmail.com>
2023-05-07 09:55:32 +0800

    added resolution options

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
+import os
 import argparse
 from PIL import Image
 from add_signature import add_signature, add_sequence_number
 from sildeshow import create_video_slideshow
 from datetime import datetime
-import os
 
-def main(input_path, output_path):
-    signed_path = output_path + 'signed/'
+def get_unique_filename(output_path, output_file):
+    base, ext = os.path.splitext(output_file)
+    counter = 0
+    while os.path.exists(os.path.join(output_path, output_file)):
+        counter += 1
+        output_file = f"{base}_{counter:02d}{ext}"
+    return output_file
 
-    if not os.path.exists(signed_path):
-        os.mkdir(signed_path)
+def main():
+    parser = argparse.ArgumentParser(description='Add signature and create a video slideshow.')
+    parser.add_argument('-input', help='Input folder containing images', default='./input/')
+    parser.add_argument('-output', help='Output folder for the signed images and video', default='./output/')
+    parser.add_argument('-duration', help='Duration of each image in the video slideshow (default: 3 seconds)', default=3, type=int)
+    parser.add_argument('-resolution', help='Resolution of the video slideshow: "HD" or "4K" (default: "HD")', default='HD', choices=['HD', '4K'])
+    args = parser.parse_args()
 
+    resolution = (1920, 1080) if args.resolution == 'HD' else (3840, 2160)
+
+    input_path = args.input
+    output_path = args.output
+    signed_path = os.path.join(output_path, 'signed/')
+
+    os.makedirs(signed_path, exist_ok=True)
+    
     image_files = [os.path.join(input_path, f) for f in os.listdir(input_path) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
     image_files.sort()
     total_files = len(image_files)
@@ -29,15 +47,10 @@ def main(input_path, output_path):
         signed_and_numbered_images.append(signed_path + formatted_seq + ".png")
 
     date_str = datetime.now().strftime("%Y%m%d")
-    output_file = f"{date_str}.mp4"
-    video = create_video_slideshow(signed_and_numbered_images, output_path + output_file, 3, resolution=(1920, 1080))
+    output_file = f"{date_str}.mp4" 
+    output_file = get_unique_filename(output_path, output_file)
+    video = create_video_slideshow(signed_and_numbered_images, output_path + output_file, args.duration, resolution)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Create a video slideshow with signatures and sequence numbers.')
-    parser.add_argument('-input', type=str, default='./input/', help='Path to the input folder containing the images.')
-    parser.add_argument('-output', type=str, default='./output/', help='Path to the output folder where the video will be saved.')
-
-    args = parser.parse_args()
-
-    main(args.input, args.output)
+    main()
--- readme.md
+++ readme.md
@@ -2,6 +2,16 @@
 
 This Python script creates a video slideshow from a collection of images. It adds a signature and a sequence number to each image, and then combines them into a video slideshow with a specified duration per slide.
 
+## Features
+
+- Adds a custom signature to the bottom left corner of each image
+- Adds a sequence number to the bottom right corner of each image
+- Creates a video slideshow with crossfade transitions between images
+- Fades in the first image from black and fades out the last image to black
+- Resizes and centers images to fit the chosen resolution (HD or 4K)
+- Provides command line arguments to customize input folder, output folder, image duration, and resolution
+- Ensures that the output video filename is unique and does not overwrite any existing file
+
 ## Dependencies
 Python 3.6 or later
 Pillow (PIL) for image manipulation
@@ -10,7 +20,7 @@ moviepy for video creation
 Install the dependencies using pip:
 
 ```bash
-pip install pillow moviepy
+pip install -r requirements.txt
 ```
 
 ## Usage
@@ -29,14 +39,23 @@ You can specify custom input and output folders using the -input and -output arg
 python main.py -input /path/to/input/folder -output /path/to/output/folder
 ```
 
-
 The slideshow video will have a default duration of 5 seconds per slide. You can customize the duration in the create_video_slideshow function call within main.py.
 
 ## Customization
 
-You can customize the signature text, font, and size in the add_signature function call within main.py.
+3. Customize the script with the following optional command line arguments:
 
-You can customize the sequence number font and size in the add_sequence_number function call within main.py.
+- `-input` - Input folder containing the images (default: `./input/`)
+- `-output` - Output folder where the signed images and video will be saved (default: `./output/`)
+- `-duration` - Duration of each image in the video in seconds (default: `3`)
+- `-resolution` - Resolution of the video, either `"HD"` (1920x1080) or `"4K"` (3840x2160) (default: `"HD"`)
+
+Example usage with command line arguments:
+
+```bash
+    python main.py -input ./input/ -output ./output/ -duration 3 -resolution "4K"
+```
+
 
 ## License
 
